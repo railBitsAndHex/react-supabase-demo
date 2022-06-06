1523361cd96ce5e4bd96b674b68dcaee47168eeb
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-06-06 17:24:51 +0800

    Init react structure

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>HELLO WORLD</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
