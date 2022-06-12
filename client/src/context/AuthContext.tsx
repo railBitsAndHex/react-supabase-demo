7109a7e5aa7cf8a9889edd223fd59857acbba8e0
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-06-12 09:49:50 +0800

    feat: Added login functionality
    - Login functionality addition (via email)
    - Routing to profile page on login
    - Error rendering if error occurs during log in process or user already
    exists

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,22 +1,23 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {
 	IAuthContext,
 	AuthStateInitial,
 	AuthPropsType,
-	TSignUp
+	TSignUp,
+	TLogin
 } from './../types/authContext.d';
-import { supabase } from '../supabaseClient';
-import { toastError } from '../utils/toastNotification';
-import { validation3, validationLen1 } from '../utils/validations';
+import {supabase} from '../supabaseClient';
+import {toastError} from '../utils/toastNotification';
+import {validation3, validationLen1} from '../utils/validations';
 
 const AuthContext = React.createContext<IAuthContext>(AuthStateInitial);
 export const useAuth = () => useContext(AuthContext);
-export const AuthProvider = ({ children }: AuthPropsType) => {
+export const AuthProvider = ({children}: AuthPropsType) => {
 	const [user, setUser] = useState<any>();
 	const [session, setSession] = useState<any>();
 	const [sessionTrigger, setSessionTrigger] = useState<boolean>(false);
 	const signup = async (credentials: TSignUp) => {
-		const { email, password, passwordCfm } = credentials;
+		const {email, password, passwordCfm} = credentials;
 		if (!validation3(email, password, passwordCfm)) {
 			toastError('Invalid params entered!');
 			return;
@@ -28,7 +29,7 @@ export const AuthProvider = ({ children }: AuthPropsType) => {
 		try {
 			// Check if exists + email confirmed first
 			try {
-				const { data, error } = await supabase
+				const {data, error} = await supabase
 					.from('puser')
 					.select('*')
 					.eq('email', email)
@@ -51,7 +52,7 @@ export const AuthProvider = ({ children }: AuthPropsType) => {
 
 			// Sign up the user
 			try {
-				const { user, session, error } = await supabase.auth.signUp({
+				const {user, session, error} = await supabase.auth.signUp({
 					email: email,
 					password: password
 				});
@@ -72,20 +73,58 @@ export const AuthProvider = ({ children }: AuthPropsType) => {
 			}
 		}
 	};
-	const login = () => {
+	const login = async (credentials: TLogin) => {
 		try {
-			console.log('login');
+			const {email, password} = credentials;
+			try {
+				const {data, error} = await supabase
+					.from('puser')
+					.select('*')
+					.eq('email', email)
+					.filter('email_confirmed_at', 'lt', 'now');
+				if (error) {
+					toastError(error.message);
+					throw new Error(error.message);
+				}
+				if (data === null || Object.keys(data).length == 0 || data.length === 0) {
+					const errMessage = 'User does not exists. Please create an account!';
+					toastError(errMessage);
+					throw new Error(errMessage);
+				}
+			} catch (error: unknown) {
+				if (error instanceof Error) {
+					throw error;
+				}
+			}
+
+			try {
+				const {user, session, error} = await supabase.auth.signIn({
+					email: email,
+					password: password
+				});
+				if (error) {
+					console.log(error.message);
+					toastError(error.message);
+					throw new Error(error.message);
+				}
+				setUser(user);
+				setSession(session);
+			} catch (error: unknown) {
+				if (error instanceof Error) {
+					toastError(error.message);
+					throw error;
+				}
+			}
 		} catch (error: unknown) {
 			if (error instanceof Error) {
+				throw error;
 			}
 		}
 	};
 
 	useEffect(() => {
 		supabase.auth.onAuthStateChange((e, session) => {
-			supabase.auth.user() !== null
-				? setUser(supabase.auth.user())
-				: setUser(undefined);
+			supabase.auth.user() !== null ? setUser(supabase.auth.user()) : setUser(undefined);
 			if (session !== null) {
 				console.log(e);
 				setSession(session);
@@ -103,7 +142,5 @@ export const AuthProvider = ({ children }: AuthPropsType) => {
 		signup,
 		login
 	};
-	return (
-		<AuthContext.Provider value={values}>{children}</AuthContext.Provider>
-	);
+	return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
