51bc502ab9e13b05ebb3c973f4dcc5a1ec38c53e
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-06-12 17:28:55 +0800

    style: Styling to homepage and navbar
    - Created navbar
    - Added styling to navbar
    - Added styling to homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { supabase } from './../supabaseClient';
 import { useAuth } from './../context/AuthContext';
+
+import "../styles/homepage.modules.css"
+import { Button } from '@chakra-ui/react';
 function Home() {
 	const { user, session } = useAuth();
+	const authState = user && session;
 	if (!(user && session)) {
 		return (
 			<>
-				<div>Welcome to homepage</div>
+				<section className='homepage-section-main'>
+					<div className='homepage-div-1'>
+						<h2 className='homepage-title'>React Supabase Demo</h2>
+						<p className='homepage-subtitle1'>Demo for using Supabase with React</p>
+						{authState ? <p>Welcome {user.email}</p> : <a href="/login"><Button className='homepage-login-rdr-btn' height={`50px`} width={'100px'} fontSize={'20px'} fontWeight={`bold`}>Login</Button></a>}
+					</div>
+				</section>
 			</>
 		);
 	}
